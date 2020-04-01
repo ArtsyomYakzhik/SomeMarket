79dc8a7e298859774b568a23066c4f59162a9ced
Тимофей Явкин <yakhzhik@gmail.com>
2020-04-01 16:46:32 +0300

    Add data models

---
+++ SomeMarket/Models/DBModels/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SomeMarket.Models.DBModels
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int OrderStatus { get; set; }
+
+        public List<CartProduct> CartProducts { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+
+    }
+}
---
+++ SomeMarket/Models/DBModels/CartProduct.cs
@@ -0,0 +1,15 @@
+﻿namespace SomeMarket.Models.DBModels
+{
+    public class CartProduct
+    {
+        public int Id { get; set; }
+
+        public int ProductQuantity  { get; set; }
+        
+        public int CartId { get; set; }
+        public Cart Cart { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
